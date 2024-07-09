e1f45e198f579d483301ea7113725209a4a1534b
kishoear <kishorear.programming@gmail.com>
2024-07-09 11:28:40 +0530

    Nav bar done

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-This is a full stack application which is used to share AI prompts with people and built using Next.js, TailwindCSS and MongoDB.
-
-[Prompt Share](www.google.com)
+This is a full stack application which is used to share AI prompts with people and built using Next.js, TailwindCSS and MongoDB.[Prompt Share](www.google.com)
--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,25 @@
+import Feed from "@components/Feed"
+
 
 const Home = () => {
     return (
-        <div>
+        <section className='w-full flex-center flex-col'>
 
-        </div>
+            <h1 className="head_text text-center">
+                Discover and Share
+                <br className="max-md:hidden" />
+
+                <span className="orange_gradient text-center"> AI-Powered prompts</span>
+            </h1>
+
+            <p className="desc text-center">
+                Promtopia is an opensource AI prompting tool used for morden world to discover,
+                create and share creative prompts.
+            </p>
+
+            <Feed />
+
+        </section>
     )
 }
 
