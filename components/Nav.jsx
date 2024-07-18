c36bdea0e3370569234cd3a2f5684e1e080eae22
kishoear <kishorear.programming@gmail.com>
2024-07-18 10:15:33 +0530

    Mobile navigation added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -10,10 +10,23 @@ const Nav = () => {
 
   const isUserLogged = true
 
+  const [Providers, setProviders] = useState(null)
+  const [Toggle, setToggle] = useState(false)
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const response = await getProviders()
+      setProviders(response)
+    }
+
+    setProvider()
+  }, [])
+
+
   return (
-   <nav className='flex-between w-full mb-16 pt-3'>
+    <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
-        <Image 
+        <Image
           src='/assets/images/logo.svg'
           alt='logo'
           width={30}
@@ -24,9 +37,8 @@ const Nav = () => {
       </Link>
 
 
-      {/* Desktop navigation : means if the screen size is above 640px apply the below property*/}
+      {/* Desktop navigation : If the screen size is above 640px apply the below property*/}
       <div className="sm:flex hidden">
-        
         {isUserLogged ? (
           <div className='flex gap-3 md:gap-5'>
             <Link href='/createPrompt' className='black_btn'>
@@ -38,27 +50,112 @@ const Nav = () => {
             </button>
 
             <Link href='/profile'>
-              <Image 
+              <Image
                 src='/assets/images/logo.svg'
                 width={37}
                 height={37}
+                alt='profile'
               />
             </Link>
 
           </div>
-        ) 
-        
-        :
+        )
+          :
+          (
+            <>
+              {Providers &&
 
-        (
-          <>
+                Object.values(Providers).map((provider) =>
+                (
+                  <button
+                    type='button'
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className='black_btn'
+                  >
+                    Sign In
+                  </button>
+                ))
 
-          </>
-        )}
+              }
+            </>
+          )}
+      </div>
+
+
+      {/* Mobile navigation : if the screen size is above 640px hide the below div*/}
+      <div className="sm:hidden flex relative">
+
+        {isUserLogged ?
+          (
+            <div>
+              <Image
+                src='/assets/images/logo.svg'
+                width={37}
+                height={37}
+                alt='profile'
+                onClick={() => setToggle((prev) => !prev)}
+              />
+
+              {Toggle &&
+                (
+                  <div className="dropdown">
+                    <Link
+                      href='/profile'
+                      className='dropdown_link w-full text-center'
+                      onClick={() => setToggle(false)}
+                    >
+                      My profile
+                    </Link>
+
+                    <Link
+                      href='/createPrompt'
+                      className='dropdown_link w-full text-center'
+                      onClick={() => setToggle(false)}
+                    >
+                      Create Prompt
+                    </Link>
+
+                    <button
+                      type='button'
+                      onClick={() => {
+                        setToggle(false)
+                        signOut()
+                      }}
+                      className='mt-5 w-full black_btn'
+                    >
+                      Sign Out
+                    </button>
+
+                  </div>
+                )
+              }
+
+            </div>
+
+          ) :
+
+          (
+            <>
+              {Providers &&
+                Object.values(Providers).map((provider) =>
+                (
+                  <button
+                    type='button'
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                    className='black_btn'
+                  >
+                    Sign In
+                  </button>
+                ))
+              }
+            </>
+          )}
 
       </div>
 
-   </nav>
+    </nav>
   )
 }
 
