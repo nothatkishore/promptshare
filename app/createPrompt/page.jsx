fbee61c25f142fea5e4135b96b2e3c538420d4e3
kishoear <kishorear.programming@gmail.com>
2024-07-20 20:41:29 +0530

    Form component added

--- app/createPrompt/page.jsx
+++ app/createPrompt/page.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+'use client'
+
+import { useState } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/router"
+
+import Form from '@components/Form'
 
 const createPrompt = () => {
+
+  const [submitting, setsubmitting] = useState(false);
+  const [post, setpost] = useState({
+    prompt: '',
+    tag: ''
+  })
+
+  const createPost = async (e) =>
+  {
+
+  }
+
   return (
-    <div>
-      <p>This is create prompt</p>
-    </div>
+    <Form 
+      type="Create"
+      post={post}
+      setpost={setpost}
+      submitting={submitting}
+      handleSubmit={createPost}
+    />
   )
 }
 
