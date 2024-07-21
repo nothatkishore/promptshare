46e9fb652f91ef5e068e0a057d0ead6e31b72e9f
kishoear <kishorear.programming@gmail.com>
2024-07-21 23:19:35 +0530

    prompt card created

--- models/prompt.js
+++ models/prompt.js
@@ -1,22 +1,20 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
 
 const PromptSchema = new Schema({
-    creator : {
-        type: Schema.Types.ObjectId,
-        ref : 'user'
-    },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  prompt: {
+    type: String,
+    required: [true, 'Prompt is required.'],
+  },
+  tag: {
+    type: String,
+    required: [true, 'Tag is required.'],
+  }
+});
 
-    prompt : {
-        type : String,
-        required : [true, 'Prompt is required']
-    },
+const Prompt = models.Prompt || model('Prompt', PromptSchema);
 
-    tag : {
-        type : String,
-        required : [true, 'Tag is required']
-    }
-})
-
-const Prompt = models.Prompt || model('Prompt', PromptSchema)
-
-export default Prompt
+export default Prompt;
--- utils/database.js
+++ utils/database.js
@@ -1,30 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let isConnected = false;    //This will allow us to track the connection status
-
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+  mongoose.set('strictQuery', true);
 
-    if (isConnected) {
-        console.log('MongoDB is already connected');
-        return;
-    }
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'sharePrompt',
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-        isConnected = true
-        console.log('MongoDB connected')
-    }
+    isConnected = true;
 
-    catch (error) {
-        console.error(error)
-    }
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-// 
