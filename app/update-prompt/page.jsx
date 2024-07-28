72fa6abe359bf165b872c9b0dcb73a4a6c406a6d
kishoear <kishorear.programming@gmail.com>
2024-07-28 11:47:04 +0530

    package.json updated

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -63,4 +63,10 @@ const UpdatePrompt = () => {
     );
 };
 
-export default UpdatePrompt;
+const UpdatePromptWrapper = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <UpdatePrompt />
+    </Suspense>
+);
+
+export default UpdatePromptWrapper;
